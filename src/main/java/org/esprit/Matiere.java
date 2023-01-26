599fb2811e9631ba73813035631663679c634db5
Amal <amal.zguem@esprit.tn>
2023-01-26 21:55:18 +0100

    constructeurmatiere

--- src/main/java/org/esprit/Matiere.java
+++ src/main/java/org/esprit/Matiere.java
@@ -4,4 +4,12 @@ public class Matiere {
  private int id;
  private String nom;
  private String description;
+ 
+ Matiere(int id, String nom, String description){
+	 this.id=id;
+	 this.nom=nom;
+	 this.description=description;}
+ 
+
+Matiere math=new Matiere(1,"Mathematiques","etude des npmbres et des formes");
 }
